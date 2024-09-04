bc4d4f77633ff8144c4cbf5efa70b33dfe8533b5
shugilbert <165976129+shugilbert@users.noreply.github.com>
2024-09-03 19:49:27 -0500

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -9,19 +9,7 @@ terraform {
 
 # Configure the AWS Provider
 provider "aws" {
-  region = "us-east-1"
+  region         = "us-east-1"
+  access_key     = "AKIASE5KRNJWJZR3VHES"
+  secret_key     = "b+VwhsIM3/g/1nr5Nf/d09vUuojmfJgbXQ6ZOUQL"
 }
-
-# Optionally, if you are using an AWS profile:
-# provider "aws" {
-#   region  = "us-east-1"
-#   profile = "your-aws-profile"  # Replace with your AWS CLI profile name
-# }
-
-# Optionally, if you want to set credentials directly (not recommended for security reasons):
-# provider "aws" {
-#   region         = "us-east-1"
-#   access_key     = "your-access-key"
-#   secret_key     = "your-secret-key"
-#   # NOTE: It's better to use environment variables or an AWS profile for credentials.
-# }
